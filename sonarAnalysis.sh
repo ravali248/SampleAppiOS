fedd54264aa38d3f355c98824c51ffa43dfbcfe4
Ravali Jumbarthi <v-rajumb@microsoft.com>
2018-11-30 12:33:31 +0530

    sonar

---
+++ sonarAnalysis.sh
@@ -0,0 +1,7 @@
+sonar-scanner \
+-Dsonar.projectKey=ravali248_SampleAppiOS \
+-Dsonar.organization=ravali248-github \
+-Dsonar.sources=. \
+-Dsonar.cfamily.build-wrapper-output=bw-output \
+-Dsonar.host.url=https://sonarcloud.io \
+-Dsonar.login=020380c302cf834ae0dc6188b83357bd28de817b
