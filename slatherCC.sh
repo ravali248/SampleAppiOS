f1638444778de967264306ff6a49956f49e87d95
Ravali Jumbarthi <v-rajumb@microsoft.com>
2018-12-01 08:26:04 +0530

    cc

--- slatherCC.sh
+++ slatherCC.sh
@@ -1,1 +1,1 @@
-slather coverage --cobertura-xml --output-directory "{Build.SourcesDirectory}" --scheme SampleAppJenkinsIOS "{Build.SourcesDirectory}"/SampleAppJenkinsIOS/SampleAppJenkinsIOS.xcodeproj/
+slather coverage --cobertura-xml --output-directory ./"{Build.SourcesDirectory}" --scheme SampleAppJenkinsIOS ./"{Build.SourcesDirectory}"/SampleAppJenkinsIOS/SampleAppJenkinsIOS.xcodeproj/
